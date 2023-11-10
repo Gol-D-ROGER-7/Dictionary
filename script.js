a1449db77bae01af3aa686e37f6496acc95fb414
Gol-D-ROGER-7 <shelbyarthur874@gmail.com>
2023-11-10 16:04:34 +0530

    style changes

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const dictionary = (word) => {
     fetch('https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary?word=' + word, options)
     .then(response => response.json())
     .then((response) => {
-        console.log(response)
+        // console.log(response)
         searchedWord.innerHTML = response.word;
         meaning.innerHTML = response.definition.replace('2.', '<br>2.').replace('3.', '<br>3.').replace('4.', '<br>4.')
     })
